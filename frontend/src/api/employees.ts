10076763a737e31bfc5bd4868a4ff0b202b1a478
cyril-ui-developer <cyril.ajieh@gmail.com>
2023-06-26 15:42:14 -0400

    add pagination

--- frontend/src/api/employees.ts
+++ frontend/src/api/employees.ts
@@ -6,7 +6,9 @@ const URLS = {
     employeesUrl: 'employees',
   };
   
-  export const getEmployees = () => {
-    return api.get<EmployeeProps[]>(URLS.employeesUrl);
+  export const fetchEmployees = () => {
+    return api.get<EmployeeProps[]>(URLS.employeesUrl).then((res) => res.data);
   };
   
+  export const fetchEmployeesByPage = (page: number) =>
+api.get<EmployeeProps>(`${URLS.employeesUrl}?_page=${page}&_limit=3`).then((res) => res)
--- frontend/src/components/tabs/EmployeesList.tsx
+++ frontend/src/components/tabs/EmployeesList.tsx
@@ -1,21 +1,44 @@
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useQuery } from 'react-query';
+import { useQuery } from "react-query";
 
-import Table, {TableData}  from "../Table";
-import { EmployeeProps } from '@api/types';
-import {
-  TableRowsProps,
-} from '@components/types';
-import { getEmployees } from "@api/employees";
+import Table, { TableData } from "../Table";
+import { EmployeeProps } from "@api/types";
+import { TableRowsProps } from "@components/types";
+import { fetchEmployeesByPage } from "@api/employees";
+import { useState } from "react";
 
+function EmployeesList() {
+  const [page, setPage] = useState(1);
 
-function EmployeesList(data: EmployeeProps) {
   const {
-    data: employees, isLoading, isSuccess, isError,
-    } = useQuery<EmployeeProps[]>('fetch-employees', getEmployees)
+    data: employees,
+    isLoading,
+    isSuccess,
+    isError,
+    isPreviousData,
+    isFetching,
+  } = useQuery<EmployeeProps[]>(
+    ["fetch-employees", page],
+    () => fetchEmployeesByPage(page),
+    { keepPreviousData: true }
+  );
 
-    console.log("employees",employees?.data)
+  console.log("employees", employees);
+
+  /*
+
+This code snippet demonstrates the Link property of the HTTP header. 
+The Link header is a string that contains a list of URLs for the different pages of data,
+ separated by commas. By splitting the Link header into an array of strings and searching
+  for the rel="prev" and rel="next" links, you can determine the URLs for the previous and next pages of data.
+  */
+  const linkHeader = employees?.headers?.get('Link');
+  const links = linkHeader?.split(',');
+  const prevLink = links?.find(l => l.includes('rel="prev"'));
+  const nextLink = links?.find(l => l.includes('rel="next"'));
+  console.log(prevLink, nextLink, isPreviousData);
+
   const tableColumns = [
     {
       key: "firstName",
@@ -44,7 +67,8 @@ function EmployeesList(data: EmployeeProps) {
     <div className="flex">
       <div className="flex-shrink-0 w-10 h-10">
         {avatarUrl ? (
-          <img alt="employee"
+          <img
+            alt="employee"
             src={avatarUrl}
             className="object-cover border-2 border-gray-350 rounded-full"
           />
@@ -91,22 +115,55 @@ function EmployeesList(data: EmployeeProps) {
     );
   };
   return (
-      <>
-            <h2 className="font-bold">Employees List</h2>
-            <div className="flex justify-center">
+    <>
+      <h2 className="font-bold">Employees List</h2>
+      <div className="flex justify-center">
         <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-          {employees?.data.length > 0 ? (
-            <Table
-              tableColumns={tableColumns}
-              TableRow={TableRow}
-              data={employees?.data}
-            />
+          {isError ? (
+            <p className="text-red-900">
+              There was a problem with fetching employees
+            </p>
+          ) : null}
+          {isLoading ? <p>Fetching employees</p> : null}
+          {isSuccess && employees?.data.length > 0 ? (
+            <>
+              <Table
+                tableColumns={tableColumns}
+                TableRow={TableRow}
+                data={employees.data}
+              />
+              <div className="flex space-x-3 items-center justify-center mt-4">
+                <button
+                  onClick={() => setPage((old) => Math.max(old - 1, 0))}
+                  className={page === 1 ? "text-gray-400" : ""}
+                  disabled={page === 1 || !prevLink}
+                >
+                  Previous
+                </button>{" "}
+                <span className="text-lg font-italic">{page}</span>
+                <button
+                  onClick={() => {
+                    if (!isPreviousData) {
+                      setPage((old) => old + 1);
+                    }
+                  }}
+                  className={
+                    (isPreviousData || !nextLink) ? "text-gray-400" : ""
+                  }
+                  // Disable the Next Page button until we know a next page is available
+                  disabled={isPreviousData || !nextLink}
+                >
+                  Next
+                </button>
+              </div>
+              {isFetching ? <span>Loading...</span> : null}{" "}
+            </>
           ) : (
-            <>No match</>
+            <>No data</>
           )}
         </div>
       </div>
-      </>
+    </>
   );
 }
 
